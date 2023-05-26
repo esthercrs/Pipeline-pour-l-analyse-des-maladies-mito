8e8a9f1c991c46905eab78edc7f406d1350e69ff
esthercrs <128157943+esthercrs@users.noreply.github.com>
2023-05-26 17:45:47 +0200

    Update Haplogrep.sh

--- Scripts/Haplogrep.sh
+++ Scripts/Haplogrep.sh
@@ -1,1 +1,3 @@
+# Auteurs : Cros Esther, Guiberteau Yaelle, Le corre Mallory, Serain Corentin et Vanney Noah
+
 ./haplogrep3 classify --in=/output/variantcall.vcf --tree=phylotree-fu-rcrs@1.2  --out=haplogroup.txt --write-qc
